9b9783c52ab999e068304fdcefc28fa271411678
EORNZ <edorenzi03@outlook.com>
2023-12-23 00:34:54 +0100

    Giocatore

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include "include/Casella.h"
-
+#include "include/Giocatore.h"
 using namespace std;
 
 int main(){
+    Casella c1('S');
+    Casella c2('L');
+
+
+    Giocatore n1(1);
+    n1.pushProprieta(c1);
+    n1.pushProprieta(c2);
+
+    cout << n1;
 
     return 0;
 }
