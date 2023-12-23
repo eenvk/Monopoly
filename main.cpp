d3e5704c73e356d8d3edee15826f0a9e4c528704
Elena <94857442+eenvk@users.noreply.github.com>
2023-12-23 13:35:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -30,6 +30,19 @@ int main(){
         }
         cout<<endl;
     }*/
+    Casella a = Casella('P');
+    Casella b = Casella('L');
+    Casella c = Casella('E');
+    cout<<a<<" "<<b<<" "<<c<<endl;
+    Giocatore g = Giocatore(0);
+    g.acquistaCasella(a);
+    g.acquistaCasa(a);
+    g.acquistaCasella(c);
+    cout<<a<<" "<<b<<" "<<c<<endl;
+    cout<<"Giocatore "<<g.getId()<<": "<<g.getProprietaPossedute()<<endl;
+    g.eliminaProprieta();
 
+    cout<<a<<" "<<b<<" "<<c<<endl;
+    cout<<"Giocatore "<<g.getId()<<": "<<g.getProprietaPossedute()<<endl;
     return 0;
 }
