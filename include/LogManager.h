2c7a1007a7f11d0e00b0f3fe57eff6e3a40667b1
eenvk <elenanovkovic2@gmail.com>
2024-01-08 00:19:23 +0100

    risolto il fatto del sovrascrivere il file log

--- include/LogManager.h
+++ include/LogManager.h
@@ -6,14 +6,26 @@
 class LogManager {
 public:
     static void log(const std::string messaggio){
+        static bool first_time = true;
         std::ofstream logFile;
-        logFile.open("log_file.txt", std::ios_base::app);
-
-        if (logFile.is_open()) {
-            logFile << messaggio << std::endl;
-            logFile.close();
-        } else {
-            std::cerr << "errore nell'apertura del file" << std::endl;
+        if(first_time){
+            logFile.open("log_file.txt", std::ios_base::out);
+            if (logFile.is_open()) {
+                logFile << messaggio << std::endl;
+                logFile.close();
+            } else {
+                std::cerr << "errore nell'apertura del file" << std::endl;
+            }
+            first_time = false;
+        }
+        else{
+            logFile.open("log_file.txt", std::ios_base::app);
+            if (logFile.is_open()) {
+                logFile << messaggio << std::endl;
+                logFile.close();
+            } else {
+                std::cerr << "errore nell'apertura del file" << std::endl;
+            }
         }
     }
 };
