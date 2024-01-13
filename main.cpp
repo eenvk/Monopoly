708cf59aa385435947b7945e11933158add52c22
eenvk <elenanovkovic2@gmail.com>
2024-01-13 18:32:27 +0100

    ..

--- main.cpp
+++ main.cpp
@@ -17,16 +17,17 @@ int main(int argc, char* argv[]){
          if(input != "human" && input != "computer"){
              std::cout << "L'argomento fornito non e' valido" << std::endl; //se inserisco qualcosa che non è human o computer
          }
+         else{
+             cout<<"Questa e' una partita di tipo "<<input<<endl;
+
+             Partita p = Partita(input);
+             p.run();
+             p.printWinner();
+         }
 
      } else {
        std::cout << "Il numero di argomenti forniti non e' corretto" << std::endl; //*
      }
-
-     cout<<"Questa e' una partita di tipo "<<input<<endl;
-
-     Partita p = Partita(input);
-     p.run();
-     p.printWinner();
 
      return 0;
 }
